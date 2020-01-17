8a4e69152e9b8db2690806eea6058e11258bbf51
Vitor Hugo <hugov1983@gmail.com>
2020-01-17 10:28:27 -0300

    Adicionado o exemplo de uso de comentário.

---
+++ 001-estudo/001-comentario.py
@@ -0,0 +1,14 @@
+# Comentário de uma linha
+print("Comentário de uma linha")
+
+# Comentário multi linha
+# Linha 2
+# Linha 3
+print("Comentário multi linha")
+
+""" 
+    Comentário multi linha
+    Linha 2
+    Linha 3 
+"""
+print("Comentário multi linha")
